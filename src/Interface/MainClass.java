09bd259723e9357e40a17bfeea10e234de1262e7
Andrew Ross <Trudgedy@gmail.com>
2018-10-06 12:03:21 +0200

    Created repository with GUIs.

---
+++ src/Interface/MainClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Heli
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HomeGUI home = new HomeGUI();
+        home.setVisible(true);
+    }
+    
+}
