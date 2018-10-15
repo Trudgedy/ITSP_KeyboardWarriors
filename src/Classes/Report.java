d8a09c8dd08bee02a922be14f1ed53b520a1e09b
Alastair <alastair@fullfacing.com>
2018-10-15 13:09:59 +0200

    Report class added, getters and setters made

---
+++ src/Classes/Report.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Alastair
+ */
+public class Report {
+    public String date;
+    public String description;
+    public String amount;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
