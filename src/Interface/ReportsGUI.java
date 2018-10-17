047ebf04b2eb2b760015a1786bdeea35cc513ed0
Liam <5al4m4nd3r@gmail.com>
2018-10-17 16:45:29 +0200

    Updated all GUIs

--- src/Interface/ReportsGUI.java
+++ src/Interface/ReportsGUI.java
@@ -29,18 +29,152 @@ public class ReportsGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnHome = new javax.swing.JButton();
-        reportTypeLabel = new javax.swing.JLabel();
-        reportTypeComboBox = new javax.swing.JComboBox<String>();
-        periodLabel = new javax.swing.JLabel();
-        periodComboBox = new javax.swing.JComboBox<String>();
-        generateReportButton = new javax.swing.JButton();
-        printButton = new javax.swing.JButton();
-        saveAsPdfButton = new javax.swing.JButton();
+        panelHeader = new javax.swing.JPanel();
+        lblCompanyName = new javax.swing.JLabel();
+        panelOptions = new javax.swing.JPanel();
+        lblReport = new javax.swing.JLabel();
+        comboReportType = new javax.swing.JComboBox<String>();
+        lblPeriod = new javax.swing.JLabel();
+        comboPeriod = new javax.swing.JComboBox<String>();
+        btnGenerateReport = new javax.swing.JButton();
+        panelReports = new javax.swing.JPanel();
         reportScrollPane = new javax.swing.JScrollPane();
-        reportTextPane = new javax.swing.JTextPane();
+        txtpaneReport = new javax.swing.JTextPane();
+        panelSavePrint = new javax.swing.JPanel();
+        btnSavePDF = new javax.swing.JButton();
+        btnPrint = new javax.swing.JButton();
+        panelFooter = new javax.swing.JPanel();
+        btnHome = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAlwaysOnTop(true);
+
+        panelHeader.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        lblCompanyName.setFont(new java.awt.Font("Arial Black", 0, 48)); // NOI18N
+        lblCompanyName.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblCompanyName.setText("Company Name");
+
+        javax.swing.GroupLayout panelHeaderLayout = new javax.swing.GroupLayout(panelHeader);
+        panelHeader.setLayout(panelHeaderLayout);
+        panelHeaderLayout.setHorizontalGroup(
+            panelHeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelHeaderLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblCompanyName, javax.swing.GroupLayout.DEFAULT_SIZE, 776, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        panelHeaderLayout.setVerticalGroup(
+            panelHeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelHeaderLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblCompanyName)
+                .addContainerGap())
+        );
+
+        panelOptions.setBorder(javax.swing.BorderFactory.createTitledBorder("Options"));
+
+        lblReport.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        lblReport.setText("Report Type:");
+
+        comboReportType.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
+        comboReportType.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Turnover", "Profit" }));
+
+        lblPeriod.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        lblPeriod.setText("Period:");
+
+        comboPeriod.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
+        comboPeriod.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Last Month", "Last Year" }));
+
+        btnGenerateReport.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
+        btnGenerateReport.setText("Generate Report");
+
+        javax.swing.GroupLayout panelOptionsLayout = new javax.swing.GroupLayout(panelOptions);
+        panelOptions.setLayout(panelOptionsLayout);
+        panelOptionsLayout.setHorizontalGroup(
+            panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelOptionsLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelOptionsLayout.createSequentialGroup()
+                        .addGroup(panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblReport)
+                            .addComponent(lblPeriod))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(comboPeriod, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(comboReportType, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(panelOptionsLayout.createSequentialGroup()
+                        .addComponent(btnGenerateReport, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        panelOptionsLayout.setVerticalGroup(
+            panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelOptionsLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblReport)
+                    .addComponent(comboReportType, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addGroup(panelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblPeriod)
+                    .addComponent(comboPeriod, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 84, Short.MAX_VALUE)
+                .addComponent(btnGenerateReport, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        panelReports.setBorder(javax.swing.BorderFactory.createTitledBorder("Report"));
+
+        txtpaneReport.setEditable(false);
+        txtpaneReport.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        txtpaneReport.setEnabled(false);
+        reportScrollPane.setViewportView(txtpaneReport);
+
+        javax.swing.GroupLayout panelReportsLayout = new javax.swing.GroupLayout(panelReports);
+        panelReports.setLayout(panelReportsLayout);
+        panelReportsLayout.setHorizontalGroup(
+            panelReportsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelReportsLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(reportScrollPane)
+                .addContainerGap())
+        );
+        panelReportsLayout.setVerticalGroup(
+            panelReportsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelReportsLayout.createSequentialGroup()
+                .addComponent(reportScrollPane)
+                .addContainerGap())
+        );
+
+        panelSavePrint.setBorder(javax.swing.BorderFactory.createTitledBorder("Save/Print"));
+
+        btnSavePDF.setText("Save as PDF");
+
+        btnPrint.setText("Print");
+
+        javax.swing.GroupLayout panelSavePrintLayout = new javax.swing.GroupLayout(panelSavePrint);
+        panelSavePrint.setLayout(panelSavePrintLayout);
+        panelSavePrintLayout.setHorizontalGroup(
+            panelSavePrintLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelSavePrintLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnSavePDF)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnPrint, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        panelSavePrintLayout.setVerticalGroup(
+            panelSavePrintLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelSavePrintLayout.createSequentialGroup()
+                .addGroup(panelSavePrintLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSavePDF, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnPrint, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        panelFooter.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
         btnHome.setText("Home");
         btnHome.addActionListener(new java.awt.event.ActionListener() {
@@ -49,81 +183,54 @@ public class ReportsGUI extends javax.swing.JFrame {
             }
         });
 
-        reportTypeLabel.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
-        reportTypeLabel.setText("Report Type:");
-
-        reportTypeComboBox.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
-        reportTypeComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Turnover", "Profit" }));
-
-        periodLabel.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
-        periodLabel.setText("Period:");
-
-        periodComboBox.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
-        periodComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Last Month", "Last Year" }));
-
-        generateReportButton.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
-        generateReportButton.setText("Generate Report");
-
-        printButton.setText("Print");
-
-        saveAsPdfButton.setText("Save as PDF");
-
-        reportTextPane.setEditable(false);
-        reportTextPane.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        reportTextPane.setEnabled(false);
-        reportScrollPane.setViewportView(reportTextPane);
+        javax.swing.GroupLayout panelFooterLayout = new javax.swing.GroupLayout(panelFooter);
+        panelFooter.setLayout(panelFooterLayout);
+        panelFooterLayout.setHorizontalGroup(
+            panelFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelFooterLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnHome)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        panelFooterLayout.setVerticalGroup(
+            panelFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFooterLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnHome)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(panelFooter, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(btnHome))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(reportTypeLabel)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(reportTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(periodLabel)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(periodComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(18, 18, 18)
-                        .addComponent(generateReportButton, javax.swing.GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(reportScrollPane)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(saveAsPdfButton)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(printButton)))))
-                .addContainerGap())
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(panelOptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(panelSavePrint, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(panelReports, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(panelHeader, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addComponent(btnHome)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(reportTypeLabel)
-                    .addComponent(reportTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(periodLabel)
-                    .addComponent(periodComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(generateReportButton, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(reportScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 318, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(printButton)
-                    .addComponent(saveAsPdfButton))
+                .addGap(15, 15, 15)
+                .addComponent(panelHeader, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(panelOptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addComponent(panelSavePrint, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(panelReports, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(panelFooter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -175,15 +282,21 @@ public class ReportsGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnGenerateReport;
     private javax.swing.JButton btnHome;
-    private javax.swing.JButton generateReportButton;
-    private javax.swing.JComboBox<String> periodComboBox;
-    private javax.swing.JLabel periodLabel;
-    private javax.swing.JButton printButton;
+    private javax.swing.JButton btnPrint;
+    private javax.swing.JButton btnSavePDF;
+    private javax.swing.JComboBox<String> comboPeriod;
+    private javax.swing.JComboBox<String> comboReportType;
+    private javax.swing.JLabel lblCompanyName;
+    private javax.swing.JLabel lblPeriod;
+    private javax.swing.JLabel lblReport;
+    private javax.swing.JPanel panelFooter;
+    private javax.swing.JPanel panelHeader;
+    private javax.swing.JPanel panelOptions;
+    private javax.swing.JPanel panelReports;
+    private javax.swing.JPanel panelSavePrint;
     private javax.swing.JScrollPane reportScrollPane;
-    private javax.swing.JTextPane reportTextPane;
-    private javax.swing.JComboBox<String> reportTypeComboBox;
-    private javax.swing.JLabel reportTypeLabel;
-    private javax.swing.JButton saveAsPdfButton;
+    private javax.swing.JTextPane txtpaneReport;
     // End of variables declaration//GEN-END:variables
 }
